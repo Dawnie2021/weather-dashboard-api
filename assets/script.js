6827cc4ae3899d5b470bdd2aca552c25fadc00c2
Dawn nguyen <dawnnguyen2021@gmail.com>
2023-11-23 14:44:43 -0500

    added another fetch request

--- assets/script.js
+++ assets/script.js
@@ -2,26 +2,36 @@
 
 // bceb14bd5cd6a234ef51a73c1cda6012
 
-// function getGeoWeather(lat, lon) {
-fetch("http://api.openweathermap.org/geo/1.0/direct?appid=d91388f9a246c15c271361c3182b51d2&limit=1&q=Chicago")
-.then(function (response) {
-    return response.json();
-})
-
- .then(function(data) {
-    console.log(data);
- })
 
 
-// function getCityGeoData() {
-//     fetch()
-//     .then(function(response) {
-//         return response.json();
 
-//     })
-//     .then (function(data) {
-//         console.log(data);
-//         getGeoWeather(data.lat, data.lon);
-//     })
-// }
-// getCityGeoData();
+function getGeoWeather(lat, lon) {
+
+
+fetch(api.openweathermap.org/data/2.5/forecast?appid=bceb14bd5cd6a234ef51a73c1cda6012lat=' + lat + '&lon=' + lon'&units=imperial')
+    .then(function(response) {
+        return response.json();
+
+    })
+    .then (function(data) {
+        console.log(data);
+        getGeoWeather(data.lat, data.lon);
+    })
+}
+
+
+function getCityGeoData() {
+fetch("http://api.openweathermap.org/geo/1.0/direct?appid=bceb14bd5cd6a234ef51a73c1cda6012&limit=1&q=Rochester")
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (data) {
+        console.log(data);
+        getGeoWeather(data[0].lat, data[0].lon);
+    })
+
+}
+
+
+getCityGeoData();
