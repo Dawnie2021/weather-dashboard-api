8e8c692c9e18f9a78682a6b32a3263ec8410fe52
Dawn nguyen <dawnnguyen2021@gmail.com>
2023-11-27 15:56:16 -0500

    created a function to get the current weather and the five day forecast

--- assets/script.js
+++ assets/script.js
@@ -26,36 +26,39 @@ function getCityGeoData() {
 
 }
 
-// Created a function to get the latitude and longitude
-function getGeoWeather(lat, lon) {
-    fetch('https://api.openweathermap.org/data/2.5/forecast?appid=bceb14bd5cd6a234ef51a73c1cda6012&lat=' + lat + '&lon=' + lon + '&units=imperial')
-        .then(function (response) {
-            return response.json();
+// get the current weather and the 5 day forecast
+function getWeatherReport(cityName, lat, lon) {
+    getCurrentWeather(cityName, lat, lon);
+    getForecast(cityName, lat, lon);
+    loadWeatherReportFromLocalStorage(cityName);
+    addSearchHistory(cityName);
+}
 
-        })
-        .then(function (data) {
 
-            while(fiveDayForecastContainer.firstChild) {
-                fiveDayForecastContainer.removeChild(fiveDayForecastContainer.firstChild);
-            }
 
-            // created a for loop to get the five days of weather data (Tutor helped with this part)
-            for (let i = 0; i < data.list.length; i += 8) {
-                var fiveDayCard = `
-            <div class="card" style="width: 12rem; padding: 5px; margin: 5px;">
-            <h5 class="card-title">${dayjs(data.list[i].dt_txt).format('M/D/YYYY')}</h5>
-            <img src="https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png" class="card-img-top" alt="...">
-            <div class = "card-body" >
-            <p class="card-text">Temp: ${data.list[i].main.temp}°F</p>
-            <p class="card-text">Speed: ${data.list[i].wind.speed}MPH</p>
-            <p class="card-text">Humidity: ${data.list[i].main.humidity}</p>
-            </div >
+
+// created a for loop to get the five days of weather data (Tutor helped with this part)
+        for (let i = 0; i < data.list.length; i += 8) {
+            var fiveDayCard = `
+            <div class="card m-1">
+                <h5 class="card-title">${dayjs(data.list[i].dt_txt).format('M/D/YYYY')}</h5>
+                <img src="https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png" class="card-img-top" alt="...">
+                <div class = "card-body" >
+                    <p class="card-text">Temp: ${data.list[i].main.temp}°F</p>
+                    <p class="card-text">Speed: ${data.list[i].wind.speed}MPH</p>
+                    <p class="card-text">Humidity: ${data.list[i].main.humidity}</p>
+                </div >
             </div >
                 `
-                var fiveDayCardDiv = document.createElement('div')
-                fiveDayCardDiv.innerHTML = fiveDayCard
-                fiveDayForecastContainer.appendChild(fiveDayCardDiv)
-            }
-        })
-}
+            var fiveDayCardDiv = document.createElement('div')
+            fiveDayCardDiv.innerHTML = fiveDayCard
+            fiveDayForecastContainer.appendChild(fiveDayCardDiv)
+        }
+   
+
+
+   
+
+
+
 
